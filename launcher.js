273de67b446ed7cc27bb4958d63715692d257a2e
spoorthyandhe <70134338+spoorthyandhe@users.noreply.github.com>
2020-09-21 20:17:47 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,30 @@
+ class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB:pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.launch= Constraint.create(options);
+        World.add(world, this.launch);
+    }
+
+    fly(){
+        this.launch.bodyA=null;
+    }
+
+    attach(body){
+        this.launch.bodyB=body;
+    }
+
+    display(){
+        if(this.launch.bodyA){
+        var pointA = this.launch.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+}
